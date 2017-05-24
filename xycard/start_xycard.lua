635730744762e75a6b24a67c2f2f5067a77b12f3
chenhw <chenhongwei1112@163.com>
2017-05-24 09:20:21 +0800

    1.添加xycard工程

---
+++ xycard/start_xycard.lua
@@ -0,0 +1,37 @@
+local skynet = require "skynet"
+local sprotoloader = require "sprotoloader"
+local snax = require "snax"
+
+local max_client = 64
+
+skynet.start(function()
+	skynet.error("Server start")
+	skynet.uniqueservice("protoloader")
+	if not skynet.getenv "daemon" then
+		local console = skynet.newservice("console")
+	end
+	skynet.newservice("debug_console",8000)
+	skynet.newservice("simpledb")
+	local watchdog = skynet.newservice("watchdog")
+	skynet.call(watchdog, "lua", "start", {
+		port = 8888,
+		maxclient = max_client,
+
+		nodelay = true,
+	})
+	skynet.error("Watchdog listen on", 8888)
+
+	local testservice = snax.newservice("xycard_service_01")
+	-- local myservice1 = skynet.newservice("myservice1")
+
+	-- local myservice1 = skynet.newservice("myservice1")
+	-- print(skynet.call(myservice1, "lua", 1, 2, 3))
+
+	-- local myservice2 = skynet.newservice("myservice2")
+	-- print(skynet.call(myservice2, "lua", 1, 2, 3))
+
+
+	-- local echo_reload = skynet.newservice("echo_reload")
+
+	skynet.exit()
+end)
