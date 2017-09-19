ff4f66bebc66338f17aa0bc8f592b68d41fef281
chenhw <chenhongwei1112@163.com>
2017-09-19 12:01:26 +0800

    1.更新代码.

--- examples/main.lua
+++ examples/main.lua
@@ -1,15 +1,12 @@
 local skynet = require "skynet"
 local sprotoloader = require "sprotoloader"
-local snax = require "snax"
+local snax = require "skynet.snax"
 
 local max_client = 64
 
 skynet.start(function()
 	skynet.error("Server start")
 	skynet.uniqueservice("protoloader")
-	if not skynet.getenv "daemon" then
-		local console = skynet.newservice("console")
-	end
 	skynet.newservice("debug_console",8000)
 	skynet.newservice("simpledb")
 	local watchdog = skynet.newservice("watchdog")
@@ -22,10 +19,9 @@ skynet.start(function()
 	skynet.error("Watchdog listen on", 8888)
 
 	local testservice = snax.uniqueservice("testservice")
-	-- local myservice1 = skynet.newservice("myservice1")
 
-	-- local myservice1 = skynet.newservice("myservice1")
-	-- print(skynet.call(myservice1, "lua", 1, 2, 3))
+	local myservice1 = skynet.newservice("myservice1")
+	--print(skynet.call(myservice1, "lua", 1, 2, 3))
 
 	-- local myservice2 = skynet.newservice("myservice2")
 	-- print(skynet.call(myservice2, "lua", 1, 2, 3))
